7a0a6ee78c2e8b201797757c1fc6d90b7ee6bb3d
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-16 15:21:51 +0100

    fit: aggunte funzioni nome e hobbies

---
+++ hobbies.js
@@ -0,0 +1,5 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+  return hobbyArray = [hobbyOne, hobbyTwo, hobbyThree]
+}
+
+module.export = hobbies(hobbyOne, hobbyTwo, hobbyThree);
---
+++ names.js
@@ -0,0 +1,5 @@
+function nomeCompleto(firstName, lastName){
+  return fullName = {"nome": firstName, "cognome": lastName}
+}
+
+module.exports = nomeCompleto(firstName, lastName);
